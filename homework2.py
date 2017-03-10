f685765894e11b878d57302b9f58b8c6dbbe370c
Ivancha-Igor <ivan4afamily@gmail.com>
2017-03-10 21:13:12 +0200

    first commit

---
+++ homework2.py
@@ -0,0 +1,25 @@
+
+# def power(num, x=1):
+# 	result = 0;
+# 	for i in range(x):
+# 		result = result + num
+# 	return result
+#
+#
+# print power(x = 5, num = 6)
+#
+#
+#
+# # function with variable number of arguments
+# def multi_add(*args):
+# 	result = 0;
+# 	for x in args:
+# 		result = result + x
+# 	return result
+#
+# print multi_add( 5, 6)
+
+
+primes = [30 - 3 * 5 + 6 / 2]
+for prime in primes:
+    print(prime)
